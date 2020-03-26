02c8fab6ed617d9d98a2cf5c228b1ee45a0c5e2e
Vincent Iroleh <irolehiroleh@gmail.com>
2020-03-26 23:43:40 +0100

    ft: add PWA to the app

--- assets/js/app.js
+++ assets/js/app.js
@@ -163,3 +163,11 @@ function filterTasks(e) {
             }
         });
 }
+
+
+// REGISTER SERVICE WORKER
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js')
+        .then(reg => console.log('service worker registered'))
+        .catch(err => console.log('service worker not registered', err));
+}
