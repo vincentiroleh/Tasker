1806023ba517472d71949e5aff1e5620c406e53e
sammychinedu2ky <sammychinedu2ky@gmail.com>
2020-03-28 03:30:45 +0100

    init

--- assets/js/app.js
+++ assets/js/app.js
@@ -167,7 +167,7 @@ function filterTasks(e) {
 
 // REGISTER SERVICE WORKER
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('/Tasker/sw.js')
         .then(reg => console.log('service worker registered'))
         .catch(err => console.log('service worker not registered', err));
 }
